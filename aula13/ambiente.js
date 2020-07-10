517aff20ab1db7d5d20c201fa9f47f25a276aa67
TheLitz <mat.moselli@hotmail.com>
2020-07-10 14:50:50 -0300

    Módulo E 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,18 @@
+console.log('Tudo bom!')
+console.log('Tudo bom!')
+console.log('Tudo bom!')        // Sem laço
+console.log('Tudo bom!')
+console.log('Tudo bom!')
+console.log('Tudo bom!')
+
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)   // Laço While
+    c++ 
+}
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)   // Laço Do
+    c++ 
+} while (c <= 6)

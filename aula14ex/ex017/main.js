eba2aea3579a2f82465ae67af5109f6e63fbc048
TheLitz <mat.moselli@hotmail.com>
2020-07-13 09:39:39 -0300

    Módulo E 14 (exercício 2 e solução)

--- aula14ex/ex017/main.js
+++ aula14ex/ex017/main.js
@@ -1,1 +1,14 @@
-alert('Olá!')
+function tabuada() {
+    var numForm = document.getElementById('num')
+    num = Number(numForm.value)
+    
+    var select = document.getElementById('select')
+
+    select.innerHTML = ''
+        for (var i = 1; i <= 10; i++) {
+            var child = document.createElement('option')
+            select.appendChild(child)
+            child.getAttribute('value',`value${i}`)
+            child.innerText += `${num} x ${i} = ` + num * i
+        }
+}
